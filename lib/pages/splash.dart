da6bd04d4f185582e9a7753022c058fa01e7e6dd
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2023-04-13 23:30:10 -0300

    criação da splash screen

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:os_manager/rotas.dart';
+import 'package:animated_splash_screen/animated_splash_screen.dart';
+import 'package:lottie/lottie.dart';
+import 'package:os_manager/pages/home.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: ElevatedButton(
-        child: const Text('Fazer Login'),
-        onPressed: () {
-          Navigator.pushNamed(context, Rotas.LOGIN);
-        },
-      ),
+    return AnimatedSplashScreen(
+      splash: Lottie.asset('assets/132689-rocket-launch.json'),
+      // Column(
+      //   children: [
+      //     const Text(
+      //       'Os Manager',
+      //       style: TextStyle(
+      //         fontSize: 40,
+      //         fontWeight: FontWeight.bold,
+      //         color: Colors.cyan
+      //         ),
+      //     ),
+      //   ],
+      // ),
+      backgroundColor: Colors.deepPurple,
+      nextScreen: const Home(),
+      duration: 4000,
     );
   }
 }
