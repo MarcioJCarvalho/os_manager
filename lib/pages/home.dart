22df5da8f238259765e10fcac32bdcd4bcc64477
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2023-04-18 17:03:48 -0300

    responsividade da home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,18 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:os_manager/menu.dart';
+import 'package:os_manager/pages/init.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return LayoutBuilder(builder: (context, constraints) {
-      final bool isMobile = constraints.maxHeight < 600;
+      final bool isMobile = constraints.maxWidth < 600;
       return Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
+          automaticallyImplyLeading: isMobile,
           title: const Text('Home'),
         ),
         drawer: const Menu(),
+        body: isMobile
+            ? const Init()
+            : Row(
+                children: [
+                  Menu(),
+                  const Expanded(child: Init()),
+                ],
+              ),
       );
     });
   }
